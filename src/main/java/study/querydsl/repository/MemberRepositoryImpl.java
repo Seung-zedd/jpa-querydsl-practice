13c6d55a457b92cbb8a4a717cf68cb48683980a7
seung-je <csz0209@gmail.com>
2025-08-30 03:05:43 +0900

    ✍️study: Querydsl 페이징 연동 학습 완료

--- src/main/java/study/querydsl/repository/MemberRepositoryImpl.java
+++ src/main/java/study/querydsl/repository/MemberRepositoryImpl.java
@@ -3,6 +3,9 @@ package study.querydsl.repository;
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import study.querydsl.dto.MemberSearchCondition;
 import study.querydsl.dto.MemberTeamDto;
 import study.querydsl.dto.QMemberTeamDto;
@@ -45,6 +48,55 @@ public class MemberRepositoryImpl implements MemberRepositoryCustom {
                 ) // 동적 쿼리
                 .fetch();
 
+    }
+
+    @Override
+    public Page<MemberTeamDto> searchPageSimple(MemberSearchCondition condition, Pageable pageable) {
+        List<MemberTeamDto> contents = getMemberTeamDtos(condition, pageable);
+
+        // count 쿼리
+        Long total = getTotal(condition);
+        return new PageImpl<>(contents, pageable, total);
+    }
+
+    private Long getTotal(MemberSearchCondition condition) {
+        return queryFactory
+                .select(member.count())
+                .from(member)
+                .leftJoin(member.team, team)
+                .where(
+                        usernameEq(condition.getUsername()),
+                        teamNameEq(condition.getTeamName()),
+                        ageGoe(condition.getAgeGoe()),
+                        ageLoe(condition.getAgeLoe())
+
+                )
+                .fetchOne();
+    }
+
+    private List<MemberTeamDto> getMemberTeamDtos(MemberSearchCondition condition, Pageable pageable) {
+        return queryFactory
+                // dto로 성능 최적화
+                .select(new QMemberTeamDto(
+                        member.id.as("memberId"),
+                        member.username,
+                        member.age,
+                        team.id.as("teamId"),
+                        team.name.as("teamName")
+
+                ))
+                .from(member)
+                .leftJoin(member.team, team)
+                .where(
+                        usernameEq(condition.getUsername()),
+                        teamNameEq(condition.getTeamName()),
+                        ageGoe(condition.getAgeGoe()),
+                        ageLoe(condition.getAgeLoe())
+
+                )
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize()) // 1 페이지당 몇개를 가져온다
+                .fetch();
     }
 
     private BooleanExpression usernameEq(String username) {
