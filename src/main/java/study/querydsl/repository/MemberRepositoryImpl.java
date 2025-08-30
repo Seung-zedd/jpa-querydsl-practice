44d8bd96d1fbf205b88fdb5e7a477a5ab9bd5be7
seung-je <csz0209@gmail.com>
2025-08-31 00:18:15 +0900

    ✍️study: 컨트롤러를 개발해서 CountQuery 최적화 완료

--- src/main/java/study/querydsl/repository/MemberRepositoryImpl.java
+++ src/main/java/study/querydsl/repository/MemberRepositoryImpl.java
@@ -1,11 +1,12 @@
 package study.querydsl.repository;
 
 import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.support.PageableExecutionUtils;
 import study.querydsl.dto.MemberSearchCondition;
 import study.querydsl.dto.MemberTeamDto;
 import study.querydsl.dto.QMemberTeamDto;
@@ -54,13 +55,8 @@ public class MemberRepositoryImpl implements MemberRepositoryCustom {
     public Page<MemberTeamDto> searchPageSimple(MemberSearchCondition condition, Pageable pageable) {
         List<MemberTeamDto> contents = getMemberTeamDtos(condition, pageable);
 
-        // count 쿼리
-        Long total = getTotal(condition);
-        return new PageImpl<>(contents, pageable, total);
-    }
-
-    private Long getTotal(MemberSearchCondition condition) {
-        return queryFactory
+        // count 쿼리 최적화
+        JPAQuery<Long> countQuery = queryFactory
                 .select(member.count())
                 .from(member)
                 .leftJoin(member.team, team)
@@ -69,9 +65,11 @@ public class MemberRepositoryImpl implements MemberRepositoryCustom {
                         teamNameEq(condition.getTeamName()),
                         ageGoe(condition.getAgeGoe()),
                         ageLoe(condition.getAgeLoe())
+                );
+        
+        return PageableExecutionUtils.getPage(contents, pageable, countQuery::fetchOne);
+//        return new PageImpl<>(contents, pageable, total);
 
-                )
-                .fetchOne();
     }
 
     private List<MemberTeamDto> getMemberTeamDtos(MemberSearchCondition condition, Pageable pageable) {
